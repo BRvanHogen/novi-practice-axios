856dbc1075a42737452d21603b2517dd08da97d9
BRvanHogen <brvanhogen@hotmail.nl>
2021-02-10 19:18:03 +0100

    created list, setup list in js

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ console.log(axios);
 
 async function displayData() {
     const response = await axios.get("https://api.spacexdata.com/v4/launches");
-   console.log(response);
+   console.log("response: ", response.data);
+   const dataList = document.getElementById("data-list");
+   console.log(dataList);
 }
 
 displayData();
